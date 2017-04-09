f54e5bf507c14115d6005a280f8c0e2806473310
Ayush Jain <jainayush975@gmail.com>
2017-04-09 21:48:08 +0530

    Add support for multiple requests using threading

--- part2.py
+++ part2.py
@@ -1,5 +1,26 @@
 import socket
 import os,time
+from threading import Thread
+
+class RequestThread(Thread):
+
+    def __init__(self, threadNo, thread_connection):
+        Thread.__init__(self)
+        self.threadNo = threadNo
+        self.connection = thread_connection
+
+    def run(self):
+        request = self.connection.recv(1024)
+        print request
+
+        request = GetRequestDict(request)
+        http_response = handleRequest(request)
+
+        #print http_response
+        self.connection.sendall(http_response)
+        self.connection.close()
+        time.sleep(60)
+        print "Thread is closing", self.threadNo
 
 def GetRequestDict(request):
     ret = dict()
@@ -15,7 +36,7 @@ def GetRequestDict(request):
         elif len(Headers) == 1:
             if "POST" in ret:
                 var = re.split('& |=', Headers[0])
-                if len(var) == 4
+                if len(var) == 4:
                     ret[var[0]] = var[1]
                     ret["filename"] = var[3]
                 #Headers = Headers.split('&')
@@ -50,17 +71,29 @@ def handleUpload(request):
 
 
 
-HOST, PORT = '', 8888
+HOST, PORT = '10.1.37.78', 9991
 
-listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-listen_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-listen_socket.bind((HOST, PORT))
-listen_socket.listen(1)
+socket_listen = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+socket_listen.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+socket_listen.bind((HOST, PORT))
+socket_listen.listen(1)
 print 'Serving HTTP on port %s ...' % PORT
 
+requestList = list()
 
 while True:
-    client_connection, client_address = listen_socket.accept()
+    client_connection, client_address = socket_listen.accept()
+
+    newRequest = RequestThread(len(requestList), client_connection)
+    newRequest.start()
+    requestList.append(newRequest)
+
+    for request in requestList:
+        if not request.isAlive():
+            requestList.remove(request)
+            request.join()
+
+    """
     pid = os.fork()
 
     if pid == 0:
@@ -80,3 +113,4 @@ while True:
 
     else:
         client_connection.close()
+    """
